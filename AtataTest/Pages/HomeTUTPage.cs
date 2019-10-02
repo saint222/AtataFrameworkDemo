edcd6085e955b35ffff4d69f9390d3ca5fc7b404
saint222 <saint12maloj@gmail.com>
2019-10-02 23:22:06 +0300

    TUTatata

---
+++ AtataTest/Pages/HomeTUTPage.cs
@@ -0,0 +1,33 @@
+﻿using Atata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtataTest.Pages
+{
+    using _ = HomeTUTPage;
+    //[Url("www.tut.by")]
+    public class HomeTUTPage : Page<_>
+    {
+        [FindByXPath("div/a[@class='enter']")]
+        public Link<_> Enter { get; set; }
+
+        [FindByName("login", OuterXPath = "//form[@class='auth-form']//")]
+        public Input<string, _> Login {get; set;}
+
+        [FindByName("password", OuterXPath = "//form[@class='auth-form']//")]
+        public PasswordInput<_> Password { get; set; }
+
+        [FindByXPath("*[contains(@class, 'button')]", OuterXPath = "//form[@class='auth-form']//")]
+        public Button<_> EnterAgain { get; set; }
+        
+        public Control<_> LoggedIn { get; set; }
+        
+        //public Control<_> ResponseCode { get; set; }
+
+        //[FindById("search_from_str")]
+        //public Input<string, _> Inp { get; set; }
+    }
+}
---
+++ AtataTest/UITestFixture.cs
@@ -0,0 +1,47 @@
+﻿using Atata;
+using AtataTest.Pages;
+using NUnit.Framework;
+
+namespace AtataTest
+{
+    [TestFixture]
+    public class UITestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // Find information about AtataContext set-up on https://atata.io/getting-started/#set-up
+            AtataContext.Configure().
+                UseChrome().
+                //    WithArguments("start-maximized").
+                UseBaseUrl("https://www.tut.by").
+                UseCulture("en-us").
+                UseNUnitTestName().
+                AddNUnitTestContextLogging().
+                LogNUnitError().
+                UseAssertionExceptionType<NUnit.Framework.AssertionException>().
+                UseNUnitAggregateAssertionStrategy().
+                Build();
+        }
+
+        [Test]
+        public void TutLogin()
+        {
+            Go.To<HomeTUTPage>()
+                .Enter.Click()
+                .Login.Set("saint12maloj@gmail.com")
+                .Password.Set("Iambored12")
+                .EnterAgain.Click()
+                //.ResponseCode.Content.Should.Contain("200")                
+                //.Inp.Set("новости")
+                .LoggedIn.Content.Should.Contain("Saint Saint")
+                .Wait(10);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+    }
+}
